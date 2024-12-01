16bb0b6f88eae6c63e7f3abda422ef33ce31de59
ManuelElias <elias.soria.juan.manuel@gmail.com>
2024-11-30 23:20:18 -0400

    redeem contract created, tested and deployed

---
+++ script/redeem.s.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Redeem} from "../src/redeem.sol";
+import {RegisterUsers} from "../src/registerUsers.sol";
+import {HelPetToken} from "../src/tokenHelPet.sol";
+
+/**
+ * @title RedeemScript
+ * @dev Deployment script for Redeem contract
+ */
+
+contract RedeemScript is Script {
+    // Contract instances
+    Redeem public redeem;
+    RegisterUsers public registerUsers;
+    HelPetToken public tokenHelPet;
+    // Address that will be set as owner and agent
+    address owner;
+
+    /**
+     * @dev Sets up initial state before deployment
+     */
+    function setUp() public {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Main deployment function
+     * Deploys Redeem contract and sets up initial configuration
+     */
+    function run() public {
+        vm.startBroadcast();
+
+        // Deploy dependency contracts first
+        registerUsers = new RegisterUsers();
+        tokenHelPet = new HelPetToken("HelPet", "HPET");
+
+        // Deploy Redeem with dependencies
+        redeem = new Redeem(address(registerUsers), address(tokenHelPet));
+        
+        // Set up initial agents
+        redeem.addAgent(owner);
+        registerUsers.addAgent(owner);
+        tokenHelPet.addAgent(owner);
+
+        vm.stopBroadcast();
+    }
+    
+}
---
+++ test/redeem.t.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Redeem} from "../src/redeem.sol";
+
+/**
+ * To run this contract, copy and paste this command in the terminal:
+ * forge test -vvvv --match-path test/redeem.t.sol --fork-url https://sepolia.basescan.org
+ * 
+ * @dev Contract deployed on Base Sepolia
+ * https://sepolia.basescan.org/address/0x5fF76F1e069F2CE71c49aF3254CBC4d95123a0CB
+*/
+
+contract RedeemTest is Test {
+    Redeem public redeem;
+    address constant owner = 0xd806A01E295386ef7a7Cea0B9DA037B242622743;
+    address constant user1 = 0x9F693ea18DA08824E729d5efc343Dd78254a9302;
+    address constant user2 = 0xFc6623B340A505E6819349aF6beE2333D31840E1;
+    address constant entity1 = 0x9b63FA365019Dd7bdF8cBED2823480F808391970;
+
+    function setUp() public {
+        redeem = Redeem(0x5fF76F1e069F2CE71c49aF3254CBC4d95123a0CB);
+    }
+
+    // ----------------------------------- Create post ----------------------------------- 
+    function test_createPost() public {
+        vm.startPrank(entity1);
+        redeem.createPost(100, 100);
+        vm.stopPrank();
+    }
+
+    function test_fail_createPost() public {
+        vm.startPrank(user1);
+        vm.expectRevert("Only registered entities can create posts");
+        redeem.createPost(100, 100);
+        vm.stopPrank();
+    }
+
+    // ----------------------------------- Close post ----------------------------------- 
+    function test_closePost() public {
+        // First create a post
+        vm.startPrank(entity1);
+        redeem.createPost(100, 100);
+        vm.stopPrank();
+
+        // Then close it
+        vm.startPrank(entity1);
+        redeem.closePost(0); // Pass post ID
+        vm.stopPrank();
+    }
+
+    function test_fail_closePost() public {
+        vm.startPrank(user1);
+        vm.expectRevert("Ownable: caller is not the owner");
+        redeem.closePost(0);
+        vm.stopPrank();
+    }
+
+    // ----------------------------------- Redeem item -----------------------------------
+    function test_redeemItem() public {
+        // First create a post
+        vm.startPrank(entity1);
+        redeem.createPost(100, 100);
+        vm.stopPrank();
+
+        // Then redeem an item
+        vm.startPrank(user1);
+        redeem.redeemItem(0); // Pass post ID
+        vm.stopPrank();
+    }
+
+    function test_fail_redeemItem() public {
+        vm.startPrank(user2);
+        vm.expectRevert("Only registered users or entities can redeem");
+        redeem.redeemItem(0);
+        vm.stopPrank();
+    }
+    
+}
